3903e3ddd6b7e5ff63bcb315023b91668f67c808
Adeline Gerard <adeline.grd@gmail.com>
2020-12-30 17:10:25 -0500

    Add Data annotations client

--- EudonetExercice/Entities/Client.cs
+++ EudonetExercice/Entities/Client.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace EudonetExercice.Entities
 {
     public class Client
     {
+        [Required]
         public string FirstName { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
         public DateTime BirthDate { get; set; }       
     }
 }
